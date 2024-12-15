975ca9f2208759322dc84b890b0bbf5854552919
Joseph Peter <josepholuwaseunpeter@yahoo.com>
2024-12-15 18:54:23 +0100

    Added IOS video play back and seek

--- shared/src/iosMain/kotlin/com/app/miva/player/presentation/screen/VideoPlayer.ios.kt
+++ shared/src/iosMain/kotlin/com/app/miva/player/presentation/screen/VideoPlayer.ios.kt
@@ -1,11 +1,16 @@
 package com.app.miva.player.presentation.screen
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.interop.UIKitView
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.UIKitView
 import kotlinx.cinterop.CValue
 import kotlinx.cinterop.ExperimentalForeignApi
 import platform.AVFoundation.AVPlayer
@@ -14,16 +19,20 @@ import platform.AVFoundation.currentItem
 import platform.AVFoundation.currentTime
 import platform.AVFoundation.pause
 import platform.AVFoundation.play
+import platform.AVFoundation.seekToTime
 import platform.AVKit.AVPlayerViewController
 import platform.CoreGraphics.CGRect
+import platform.CoreMedia.CMTimeGetSeconds
+import platform.CoreMedia.CMTimeMakeWithSeconds
 import platform.Foundation.NSURL
 import platform.QuartzCore.CATransaction
 import platform.QuartzCore.kCATransactionDisableActions
 import platform.UIKit.UIView
+import platform.darwin.Float64
 
 @OptIn(ExperimentalForeignApi::class)
 @Composable
-actual fun videoPlayer(modifier: Modifier, url: String) : VideoPlayerControl {
+actual fun videoPlayer(modifier: Modifier, url: String): VideoPlayerControl {
     val player = remember { AVPlayer(uRL = NSURL.URLWithString(url)!!) }
     val playerLayer = remember { AVPlayerLayer() }
     val avPlayerViewController = remember { AVPlayerViewController() }
@@ -36,32 +45,20 @@ actual fun videoPlayer(modifier: Modifier, url: String) : VideoPlayerControl {
 
     UIKitView(
         factory = {
-            // Create a UIView to hold the AVPlayerLayer
-            val playerContainer = UIView()
-            playerContainer.addSubview(avPlayerViewController.view)
-            // Return the playerContainer as the root UIView
-            playerContainer
-        },
-        onResize = { view: UIView, rect: CValue<CGRect> ->
-            CATransaction.begin()
-            CATransaction.setValue(true, kCATransactionDisableActions)
-            view.layer.setFrame(rect)
-            playerLayer.setFrame(rect)
-            avPlayerViewController.view.layer.frame = rect
-            CATransaction.commit()
+            avPlayerViewController.view
         },
         update = { view ->
             player.play()
             avPlayerViewController.player!!.play()
         },
-        modifier = modifier
+        modifier = modifier.height(200.dp)
     )
 
     return videoControl
 }
 
 class IOSVideoPlayerControl : VideoPlayerControl {
-    private var player : AVPlayer? = null
+    private var player: AVPlayer? = null
 
     override fun play() {
         player?.play()
@@ -71,11 +68,16 @@ class IOSVideoPlayerControl : VideoPlayerControl {
         player?.pause()
     }
 
+    @OptIn(ExperimentalForeignApi::class)
     override fun seek(position: Int) {
+        val seekTime = CMTimeMakeWithSeconds(position.toDouble(), 600)
+        player?.seekToTime(seekTime)
     }
 
+    @OptIn(ExperimentalForeignApi::class)
     override fun currentPosition(): Int {
-        return 0
+        val time = player?.currentItem?.currentTime()
+        return time?.let { CMTimeGetSeconds(it).toInt() } ?: 0
     }
 
     fun setVideoPlayer(player: AVPlayer) {
